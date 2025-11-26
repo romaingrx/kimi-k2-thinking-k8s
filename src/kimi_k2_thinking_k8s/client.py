9078c63e2b50cb048c5546abeffbd8692dbe6221
Romain Graux <48758915+romaingrx@users.noreply.github.com>
2025-11-25 17:23:12 -0800

    feat(tp16): move to tensor parallelism 16 shared with infiniband

--- src/kimi_k2_thinking_k8s/client.py
+++ src/kimi_k2_thinking_k8s/client.py
@@ -1,29 +1,22 @@
 #!/usr/bin/env python3
 
 import asyncio
+import time
 
 from openai import AsyncOpenAI
 
 
-async def test_tool_call(client: AsyncOpenAI, model: str):
-    """Test tool calling capability."""
-
-    question = """Two questions, answer quickly:
-1. What model are you?
-2. What does METR (organization) stand for?
-    """
-
-    response = await client.chat.completions.create(
-        model=model,
-        messages=[{"role": "user", "content": question}],
-        tools=[],
-        max_tokens=1500,
-    )
-
-    message = response.choices[0].message
-    print(f"Question:\n\033[96m{question}\033[0m")
-    print(f"Reasoning:\n\033[94m{message.reasoning}\033[0m")  # pyright: ignore[reportUnknownMemberType, reportAttributeAccessIssue]
-    print(f"Result:\n\033[92m{message.content}\033[0m")
+async def test_tool_call(
+    client: AsyncOpenAI, question: str, model: str, semaphore: asyncio.Semaphore
+):
+    async with semaphore:
+        response = await client.chat.completions.create(
+            model=model,
+            messages=[{"role": "user", "content": question}],
+            tools=[],
+            max_tokens=1500,
+        )
+    return response
 
 
 async def main():
@@ -38,9 +31,30 @@ async def main():
 
     print(f"Connecting to {args.url}\n")
 
-    await test_tool_call(client, args.model)
+    n_parallel = 50
+    semaphore = asyncio.Semaphore(n_parallel)
 
-    print("Done!")
+    question = """Two questions, answer quickly:
+1. What model are you?
+2. What does METR (organization) stand for?
+    """
+
+    start = time.time()
+    responses = await asyncio.gather(
+        *[
+            test_tool_call(client, question, args.model, semaphore)
+            for _ in range(n_parallel)
+        ]
+    )
+    stop = time.time()
+
+    for response in responses[:5]:
+        message = response.choices[0].message
+        print(f"Question:\n\033[96m{question}\033[0m")
+        print(f"Reasoning:\n\033[94m{message.reasoning}\033[0m")  # pyright: ignore[reportUnknownMemberType, reportAttributeAccessIssue]
+        print(f"Result:\n\033[92m{message.content}\033[0m")
+
+    print(f"Time taken: {stop - start:.2f} seconds")
 
 
 if __name__ == "__main__":
